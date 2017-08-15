9971511e1239bcf946c4de9fb83d2089cb56e453
nsaito-gmo-ins <nsaito-gmo-ins>
2017-08-15 13:43:50 +0900

    posts

--- graph_traversal/tree.rb
+++ graph_traversal/tree.rb
@@ -20,12 +20,48 @@ class Tree
 
   # bfs(幅優先探索)
   def bfs(num)
-    return true if @value = num
-    @children.each {|c|
-      return true if c.bfs(num)
-    }
+    queue = []
+    queue.push(num)
+    while(queue.size!=0)
+      self = queue.shift
+      puts @value
+      @children.each do |child|
+        queue.push(child)
+      end
+    end
+  end
+end
+
+t1 = Tree.new(1,[
+  Tree.new(2)
+  Tree.new(3)
+  Tree.new(4,[
+    Tree.new(5)
+    Tree.new(6)
+  ])
+])
+t1.dfs(5)
+t1.bfs(5)
+
+
+
+class Tree
+  attr_accessor :value,:children
+
+  def initialize(value,children=[])
+    @value = value
+    @children = children
+  end
+
+  def dfs(num)
+    # 根のvalueに一致
+    return true if num = @value
+    @children.each do |child|
+      return true child.dfs(num)
+    end
     false
   end
+
 end
 
 t1 = Tree.new(1,[
