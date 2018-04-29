0c4c4a9bf29c2c452d2a83bf13cea6a8bb8c7219
oshou <adf1985adf@gmail.com>
2018-04-29 22:51:37 +0900

    add sorts by python

--- sort/quick_sort.rb
+++
@@ -1,9 +0,0 @@
-class Array
-  def quick_sort
-    return self if length <= 1
-    base = pop
-    smaller,bigger = partition{|e| e < base}
-    push base
-    smaller.quick_sort + [base] + bigger.quick_sort
-  end
-end
--- sorts/merge_sort.py
+++ sorts/merge_sort.py
@@ -2,8 +2,26 @@
 # -*- coding:utf-8
 
 def merge_sort(arr):
-    length = len(arr)
-    if length  > 1:
+    if len(arr) > 1:
+        mid = len(arr) / 2
+        left = merge_sort(arr[:mid])
+        right = merge_sort(arr[mid:])
+        arr = []
 
+        while len(left) != 0 and len(right) != 0:
+            if left[0] < right[0]:
+                arr.apend(left.pop(0))
+            else:
+                arr.apend(right.pop(0))
 
+            if len(left) != 0:
+                arr.extend(left)
 
+            if len(right) != 0:
+                arr.extend(right)
+
+        return arr
+
+if __name__ == '__main__':
+    unsorted = [1,8,5,3,126,33,25,26,35,14,52]
+    print(merge_sort(unsorted))
